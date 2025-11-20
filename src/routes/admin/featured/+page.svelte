32e1cb70578b08e7ee4cbeb708fc10cc9210acf0
manish-aeka <manish@aekaadvisors.com>
2025-11-20 16:20:44 +0530

    make a common component

--- src/routes/admin/featured/+page.svelte
+++ src/routes/admin/featured/+page.svelte
@@ -1,6 +1,16 @@
 <script>
+    import Common from "$lib/components/Admin/common.svelte";
+    import { photographyStore } from "$lib/stores/photography";
+    let pageName = "Featured";
+    let images = $state(
+        $photographyStore
+            ?.find((item) => item.title === pageName)
+            .urls.map((url, i) => ({
+                id: i,
+                url,
+                alt: "",
+            })),
+    );
 </script>
 
-<div class="bg-primary h-screen w-full">
-    <h1 class="text-white">featured Page</h1>
-</div>
+<Common {pageName} {images} />
